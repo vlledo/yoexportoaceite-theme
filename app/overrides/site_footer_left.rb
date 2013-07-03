fb53ff105dc0df516d6c4096a2d6223dc6aa0e9c
Víctor <victejor@gmail.com>
2013-07-03 13:56:19 +0200

    media queries

--- app/overrides/site_footer_left.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/shared/_footer",
-                     :name => "site_footer_left",
-                     :replace => "#footer-left[data-hook] > p:first-child",
-                     :erb => %q{<p>Theme desarrollado para <a href="http://www.yoexportoaceite.com">YoExportoAceite</a></p>},
-                     :disabled => false)
